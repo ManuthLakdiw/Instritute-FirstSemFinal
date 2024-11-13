c6ae80bbcbd478cdc83cbbaa90e7fc3eed92b20e
manuthlakdiv <manuthlakdiv2006@gmail.com>
2024-11-13 20:34:44 +0530

    created subjectName Regex

--- src/main/java/lk/ijse/gdse/instritutefirstsemfinal/controller/SubjectFormController.java
+++ src/main/java/lk/ijse/gdse/instritutefirstsemfinal/controller/SubjectFormController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -33,64 +34,13 @@ public class SubjectFormController implements Initializable {
     }
 
     SubjectModel model = new SubjectModel();
+
+    String subjectId;
+    String subjectName;
+    String subjectDescription;
+
+
     String subjectRegex = "^[A-Za-z][A-Za-z0-9 .,_]*$";
-
-    @FXML
-    private Button btnDelete;
-
-    @FXML
-    private Button btnReset;
-
-    @FXML
-    private Button btnSave;
-
-    @FXML
-    private Button btnUpdate;
-
-    @FXML
-    private TableColumn<SubjectTm, String> colDescription;
-
-    @FXML
-    private TableColumn<SubjectTm, String> colID;
-
-    @FXML
-    private TableColumn<SubjectTm, String> colName;
-
-    @FXML
-    private Label lblSubID;
-
-    @FXML
-    private Pane subjectPane;
-
-    @FXML
-    private JFXTextArea tareaDescription;
-
-    @FXML
-    private TableView<SubjectTm> tblSubject;
-
-    @FXML
-    private TextField txtSubName;
-
-    @FXML
-    void btnDeleteOnClicked(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnResetOnClicked(ActionEvent event) {
-        refreshPage();
-
-    }
-
-    @FXML
-    void btnSaveOnClicked(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnUpdateOnClicked(ActionEvent event) {
-
-    }
 
     @FXML
     void tblSubjectOnClicked(MouseEvent event) {
@@ -144,21 +94,114 @@ public class SubjectFormController implements Initializable {
 
     }
 
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<SubjectTm, String> colDescription;
+
+    @FXML
+    private TableColumn<SubjectTm, String> colID;
+
+    @FXML
+    private TableColumn<SubjectTm, String> colName;
+
+    @FXML
+    private Label lblSubID;
+
+    @FXML
+    private Label lblSubName;
+
+    @FXML
+    private Pane subjectPane;
+
+    @FXML
+    private JFXTextArea tareaDescription;
+
+    @FXML
+    private TableView<SubjectTm> tblSubject;
+
+    @FXML
+    private TextField txtSubName;
+
+    @FXML
+    void btnDeleteOnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnResetOnClicked(ActionEvent event) {
+        refreshPage();
+
+    }
+
+    @FXML
+    void btnSaveOnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnClicked(ActionEvent event) {
+
+    }
+
+
+
     public void tareaDescriptionOnKeyPressed(KeyEvent keyEvent) {
+        if (tareaDescription.getText().isEmpty()) {
+            if (keyEvent.getCode() == KeyCode.LEFT) {
+                txtSubName.requestFocus();
+                txtSubName.positionCaret(txtSubName.getText().length());
+            }
+        }
     }
 
 
 
     @FXML
     void txtSubNameOnKeyPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            tareaDescription.requestFocus();
+            tareaDescription.positionCaret(tareaDescription.getText().length());
+        }
 
     }
 
     @FXML
     void txtSubNameOnKeyTyped(KeyEvent event) {
+        subjectName = txtSubName.getText();
+        btnReset.setDisable(false);
+        RegexUtil.resetStyle(txtSubName);
 
+        if (subjectName.isEmpty()) {
+            btnReset.setDisable(true);
+            RegexUtil.resetStyle(txtSubName);
+            lblSubName.setText("");
+//            checkFieldsEmpty();
+        }else {
+            btnReset.setDisable(false);
+            if (!subjectName.matches(subjectRegex)) {
+                lblSubName.setStyle("-fx-text-fill: red");
+                RegexUtil.setErrorStyle(false,txtSubName);
+                lblSubName.setText("Start with a letter, use only letters, numbers, spaces");
+            }else {
+                lblSubName.setText(" ");
+                RegexUtil.resetStyle(txtSubName);
+            }
+        }
     }
 
-
-
 }
+
+
+
+
