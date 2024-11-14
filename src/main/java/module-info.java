a7f48262f903b2a3504ac40d95982f0469a745be
manuthlakdiv <manuthlakdiv2006@gmail.com>
2024-11-14 12:51:42 +0530

    requires controlFx dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,13 @@
 module lk.ijse.gdse.instritutefirstsemfinal {
-    requires javafx.controls;
     requires javafx.fxml;
 
     requires com.jfoenix;
     requires lombok;
     requires java.sql;
     requires org.kordamp.ikonli.javafx;
-    requires java.desktop;
-    requires javafx.media;
     requires java.mail;
+    requires mysql.connector.j;
+    requires org.controlsfx.controls;
 
     opens lk.ijse.gdse.instritutefirstsemfinal.dto.tm to javafx.base;
     opens lk.ijse.gdse.instritutefirstsemfinal.controller to javafx.fxml;
