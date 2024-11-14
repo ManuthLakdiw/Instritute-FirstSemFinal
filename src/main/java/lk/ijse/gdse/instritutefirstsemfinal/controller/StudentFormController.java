6bce48c36c6d9d28db6b56d7e01d074c190a503a
manuthlakdiv <manuthlakdiv2006@gmail.com>
2024-11-14 16:52:36 +0530

    updated

--- src/main/java/lk/ijse/gdse/instritutefirstsemfinal/controller/StudentFormController.java
+++ src/main/java/lk/ijse/gdse/instritutefirstsemfinal/controller/StudentFormController.java
@@ -1,5 +1,20 @@
 package lk.ijse.gdse.instritutefirstsemfinal.controller;
 
-public class StudentFormController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import org.controlsfx.control.CheckComboBox;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StudentFormController implements Initializable {
+
+    @FXML
+    private CheckComboBox<String> cmbCheckComboBox;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        cmbCheckComboBox.getItems().addAll("Apple", "Banana", "Cherry", "Date", "Elderberry");
+    }
 }
