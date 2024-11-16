0d2b45a25e3314cdb4bf58def1675a08358e8c83
manuthlakdiv <manuthlakdiv2006@gmail.com>
2024-11-16 17:21:25 +0530

    add confirmAlert

--- src/main/java/lk/ijse/gdse/instritutefirstsemfinal/util/AlertUtil.java
+++ src/main/java/lk/ijse/gdse/instritutefirstsemfinal/util/AlertUtil.java
@@ -1,9 +1,13 @@
 package lk.ijse.gdse.instritutefirstsemfinal.util;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.animation.PauseTransition;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DialogPane;
 import javafx.util.Duration;
+
+import java.util.Optional;
 
 public class AlertUtil {
      public static void errorAlert(Class currentClass, String header, String content) {
@@ -38,5 +42,18 @@ public class AlertUtil {
          alert.showAndWait();
 
      }
+
+    public static Optional<ButtonType> ConfirmationAlert( String message, ButtonType... buttonTypes) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, message, buttonTypes);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText(null);
+
+
+        alert.getDialogPane().getStylesheets().add(
+                AlertUtil.class.getResource("/style/Style.css").toExternalForm()
+        );
+
+        return alert.showAndWait();
+    }
 
 }
